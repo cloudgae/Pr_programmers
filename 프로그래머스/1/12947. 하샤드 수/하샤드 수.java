07368df19b20f41021ac24af88c36e8fb291a8c6
dajeong <132073732+cloudgae@users.noreply.github.com>
2025-02-20 23:36:47 +0900

    [level 1] Title: 하샤드 수, Time: 0.19 ms, Memory: 86.8 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -1,18 +1,10 @@
 class Solution {
     public boolean solution(int x) {
-        boolean answer = true;
-        int hap =0;
-        hap += x/10000;
-        hap += x%10000/1000;
-        hap += x%1000/100;
-        hap += x%100/10;
-        hap += x%10;
-        
-        if(x%hap==0){
-            answer=true;
-        }else{
-            answer=false;
+        String[] s = String.valueOf(x).split("");
+        int sum = 0;
+        for(String str : s){
+            sum += Integer.parseInt(str);
         }
-        return answer;
+        return (x%sum==0)?true:false;
     }
 }
