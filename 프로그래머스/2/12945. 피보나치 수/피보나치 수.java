feed06a273bb1b4f90e9f413b2005c10ca27d528
dajeong <132073732+cloudgae@users.noreply.github.com>
2025-03-03 22:26:16 +0900

    [level 2] Title: 피보나치 수, Time: 1.77 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -14,14 +14,4 @@ class Solution {
         }
         return answer;
     }
-    
-    public int func(int n){
-        if(n==0){
-            return 0;
-        }else if(n==1){
-            return 1;
-        }else{
-            return func(n-1)+func(n-2);
-        }
-    }
 }
